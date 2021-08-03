a800890cba298509054bc9ff2cdd21d1d651cec2
Pablo Gómez <contact@pablo-gomez.net>
2021-08-03 15:56:39 +0200

    Apply suggestions from code review
    Co-authored-by: Håvard Hem Toftevaag <70584234+htoftevaag@users.noreply.github.com>

--- nidn/trcwa/constants.py
+++ nidn/trcwa/constants.py
@@ -1,6 +1,4 @@
-# We can probably steal the code in ConvertUnits from S4 for this purpose:
-# S4.ConvertUnits(value, from_units, to_units)
-# NB: Here, we assume that the standard is micrometre (um).
+# NB: Here, we assume that the standard length unit is 1 micrometer (um).
 _TRCWA_UNIT_MAGNITUDE = 10 ** (-6)
 
 # m /s
