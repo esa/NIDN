fff45a692f362b2ad583c0a6d4450fecb7f401b7
Pablo Gómez <contact@pablo-gomez.net>
2021-08-05 11:11:09 +0200

    - Cleaned up plot_spectra - Added test to validate cfg - Improved validate_config - Small type fixes in default_cfg

--- nidn/plots/plot_spectra.py
+++ nidn/plots/plot_spectra.py
@@ -7,7 +7,27 @@ from ..trcwa.compute_spectrum import compute_spectrum
 from ..training.model.model_to_eps_grid import model_to_eps_grid
 
 
-def plot_spectra(model, run_cfg,target_R_spectrum,target_T_spectrum):
+def _add_plot(
+    fig, target_frequencies, produced_spectrum, target_spectrum, ylimits, nr, type_name
+):
+    ax = fig.add_subplot(nr)
+    ax.plot(
+        freq_to_wl(target_frequencies), produced_spectrum, marker=6, c="cornflowerblue"
+    )
+    ax.plot(freq_to_wl(target_frequencies), target_spectrum, marker=7, c="limegreen")
+    ax.legend([f"Produced {type_name}", f"Target {type_name}"], loc="lower center")
+    ax.set_xlabel("Wavelength [µm]")
+    ax.set_ylabel(f"{type_name}")
+    ax.set_xscale("log")
+    ax.xaxis.set_major_formatter(FormatStrFormatter("%.1f"))
+    # ax.xaxis.set_minor_formatter(FormatStrFormatter("%.1f"))
+    ax.axhspan(-6, 0, facecolor="gray", alpha=0.3)
+    ax.axhspan(1, 5, facecolor="gray", alpha=0.3)
+    ax.set_ylim(ylimits)
+    return fig
+
+
+def plot_spectra(model, run_cfg, target_R_spectrum, target_T_spectrum):
     """Plots the produced RTA spectra together with the target spectra.
 
     Args:
@@ -16,16 +36,22 @@ def plot_spectra(model, run_cfg,target_R_spectrum,target_T_spectrum):
         target_R_spectrum (torch.tensor): The target reflection spectrum.
         target_T_spectrum (torch.tensor): The target transmission spectrum.
     """
-    
+
+    # Create epsilon grid from the model
     eps = model_to_eps_grid(model, run_cfg)
-    prod_R_spectrum,prod_T_spectrum = compute_spectrum(eps,run_cfg)
+
+    # Compute the spectra for the given epsilon values
+    prod_R_spectrum, prod_T_spectrum = compute_spectrum(eps, run_cfg)
+
     target_frequencies = run_cfg.target_frequencies
 
+    # Convert the spectra to numpy arrays for matplotlib
     prod_R_spectrum = torch.tensor(prod_R_spectrum).detach().cpu().numpy()
     prod_T_spectrum = torch.tensor(prod_T_spectrum).detach().cpu().numpy()
     target_R_spectrum = torch.tensor(target_R_spectrum).detach().cpu().numpy()
     target_T_spectrum = torch.tensor(target_T_spectrum).detach().cpu().numpy()
 
+    # Compute absorption spectra
     prod_A_spectrum = np.ones_like(np.asarray(prod_R_spectrum)) - (
         np.asarray(prod_T_spectrum) + np.asarray(prod_R_spectrum)
     )
@@ -42,59 +68,39 @@ def plot_spectra(model, run_cfg,target_R_spectrum,target_T_spectrum):
     ):
         ylimits = [
             min(min(prod_A_spectrum), min(prod_T_spectrum), min(prod_R_spectrum))
-            - 0.225,
+            + ylimits[0],
             max(max(prod_A_spectrum), max(prod_T_spectrum), max(prod_R_spectrum)) + 0.1,
         ]
 
     fig = plt.figure(figsize=(15, 5), dpi=150)
     fig.patch.set_facecolor("white")
 
-    ax = fig.add_subplot(131)
-    ax.plot(
-        freq_to_wl(target_frequencies), prod_R_spectrum, marker=6, c="cornflowerblue"
+    fig = _add_plot(
+        fig,
+        target_frequencies,
+        prod_R_spectrum,
+        target_R_spectrum,
+        ylimits,
+        131,
+        "Reflectance",
     )
-    ax.plot(freq_to_wl(target_frequencies), target_R_spectrum, marker=7, c="limegreen")
-    ax.legend(["Produced Reflectance", "Target Reflectance"], loc="lower center")
-    ax.set_xlabel("Wavelength [µm]")
-    ax.set_ylabel("Reflectance")
-    ax.set_xscale("log")
-    ax.xaxis.set_major_formatter(FormatStrFormatter("%.1f"))
-    # ax.xaxis.set_minor_formatter(FormatStrFormatter("%.1f"))
-    ax.axhspan(-6, 0, facecolor="gray", alpha=0.3)
-    ax.axhspan(1, 5, facecolor="gray", alpha=0.3)
-    ax.set_ylim(ylimits)
-
-    ax = fig.add_subplot(132)
-    ax.plot(
-        freq_to_wl(target_frequencies), prod_T_spectrum, marker=6, c="cornflowerblue"
+    fig = _add_plot(
+        fig,
+        target_frequencies,
+        prod_T_spectrum,
+        target_T_spectrum,
+        ylimits,
+        132,
+        "Transmittance",
     )
-    ax.plot(freq_to_wl(target_frequencies), target_T_spectrum, marker=7, c="limegreen")
-    ax.legend(["Produced Transmittance", "Target Transmittance"], loc="lower center")
-    ax.set_xlabel("Wavelength [µm]")
-    ax.set_ylabel("Transmittance")
-    ax.set_xscale("log")
-    ax.xaxis.set_major_formatter(FormatStrFormatter("%.1f"))
-
-    # ax.xaxis.set_minor_formatter(FormatStrFormatter("%.1f"))
-    ax.axhspan(-6, 0, facecolor="gray", alpha=0.3)
-    ax.axhspan(1, 5, facecolor="gray", alpha=0.3)
-    ax.set_ylim(ylimits)
-
-    ax = fig.add_subplot(133)
-    ax.plot(
-        freq_to_wl(target_frequencies), prod_A_spectrum, marker=6, c="cornflowerblue"
+    fig = _add_plot(
+        fig,
+        target_frequencies,
+        prod_A_spectrum,
+        target_A_spectrum,
+        ylimits,
+        133,
+        "Absorptance",
     )
-    ax.plot(freq_to_wl(target_frequencies), target_A_spectrum, marker=7, c="limegreen")
-
-    ax.legend(["Produced Absorptance", "Target Absorptance"], loc="lower center")
-    ax.set_xlabel("Wavelength [µm]")
-    ax.set_ylabel("Absorptance")
-    ax.set_xscale("log")
-    ax.xaxis.set_major_formatter(FormatStrFormatter("%.1f"))
-
-    # ax.xaxis.set_minor_formatter(FormatStrFormatter("%.1f"))
-    ax.axhspan(-6, 0, facecolor="gray", alpha=0.3)
-    ax.axhspan(1, 5, facecolor="gray", alpha=0.3)
-    ax.set_ylim(ylimits)
 
     plt.tight_layout()
--- nidn/training/utils/validate_config.py
+++ nidn/training/utils/validate_config.py
@@ -7,6 +7,8 @@ def _validate_config(cfg: DotMap):
     Args:
         cfg (DotMap): Run config you intend to use
     """
+
+    # Check that all required entries are in the config
     required_keys = [
         "seed",
         "eps_oversampling",
@@ -34,3 +36,73 @@ def _validate_config(cfg: DotMap):
     for key in required_keys:
         if key not in cfg:
             raise ValueError(f"Missing required key: {key}")
+
+    # Check that all values are of correct type
+    integer_keys = [
+        "Nx",
+        "Ny",
+        "N_layers",
+        "N_freq",
+        "encoding_dim",
+        "out_features",
+        "n_neurons",
+        "eps_oversampling",
+        "seed",
+    ]
+    float_keys = [
+        "L",
+        "real_min_eps",
+        "real_max_eps",
+        "imag_min_eps",
+        "imag_max_eps",
+        "siren_omega",
+    ]
+    boolean_keys = []
+    string_keys = [
+        "model_type",
+    ]
+    for key in integer_keys:
+        if not isinstance(cfg[key], int):
+            raise ValueError(f"{key} must be an integer")
+
+    for key in float_keys:
+        if not isinstance(cfg[key], float):
+            raise ValueError(f"{key} must be a float")
+
+    for key in boolean_keys:
+        if not isinstance(cfg[key], bool):
+            raise ValueError(f"{key} must be a boolean")
+
+    for key in string_keys:
+        if not isinstance(cfg[key], str):
+            raise ValueError(f"{key} must be a string")
+
+    # Check that all values are within the correct ranges
+    if not (cfg.real_min_eps <= cfg.real_max_eps):
+        raise ValueError(f"real_min_eps must be less than or equal to real_max_eps")
+
+    if not (cfg.imag_min_eps <= cfg.imag_max_eps):
+        raise ValueError(f"imag_min_eps must be less than or equal to imag_max_eps")
+
+    if not (cfg.physical_wavelength_range[0] < cfg.physical_wavelength_range[1]):
+        raise ValueError(f"physical_wavelength_range must be ordered from low to high")
+
+    positive_value_keys = [
+        "L",
+        "n_neurons",
+        "Nx",
+        "Ny",
+        "N_layers",
+        "N_freq",
+        "learning_rate",
+        "iterations",
+        "eps_oversampling",
+    ]
+    for key in positive_value_keys:
+        if not (cfg[key] > 0):
+            raise ValueError(f"{key} must be a positive integer")
+
+    if not cfg.Nx % cfg.eps_oversampling == 0:
+        raise ValueError(f"Nx must be a multiple of eps_oversampling")
+    if not cfg.Ny % cfg.eps_oversampling == 0:
+        raise ValueError(f"Ny must be a multiple of eps_oversampling")
