1ceb3c0f5894e2136958fa803af29fa14efc4ee5
Pablo Gómez <contact@pablo-gomez.net>
2021-08-24 14:53:28 +0200

    Added automatic baseline detection in eps per point plot and fixed WL bug

---
+++ nidn/materials/find_closest_material.py
@@ -0,0 +1,37 @@
+from .material_collection import MaterialCollection
+
+import torch
+
+
+def _find_closest_material(eps, run_cfg):
+    """Finds the closest matching material and distance to that (in epsilon)
+    from a given epsilon grid.
+    
+    Args:
+        eps (torch.tensor): A tensor of epsilon values (Nx,Ny,N_layers,N_freq).
+        run_cfg (DotMap): Run configuration.
+    
+    Returns:
+        tuple: The closest materials and distances to that.
+    """
+    Nx, Ny, N_layers, N_freq = run_cfg.Nx, run_cfg.Ny, run_cfg.N_layers, run_cfg.N_freq
+
+    # Initiate material collection
+    material_collection = MaterialCollection(run_cfg.target_frequencies)
+
+    comparisons = torch.zeros(
+        [Nx, Ny, N_layers, N_freq, material_collection.N_materials,]
+    )
+
+    # Compute differences for all materials
+    for idx, material_name in enumerate(material_collection.material_names):
+        material_eps = material_collection[material_name]
+
+        comparisons[..., idx] = torch.abs(eps - material_eps)
+
+    # Find minimal entries
+    comparisons = comparisons.mean(dim=3)  # Average over frequencies
+    minimal_comparisons, indices = torch.min(comparisons, dim=-1)
+
+    error = torch.mean(minimal_comparisons)
+    return error, indices
--- nidn/plots/plot_eps_per_point.py
+++ nidn/plots/plot_eps_per_point.py
@@ -1,5 +1,6 @@
 import os
 import inspect
+import torch
 
 from matplotlib import pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
@@ -7,6 +8,7 @@ from ..utils.convert_units import freq_to_wl
 from ..training.model.model_to_eps_grid import model_to_eps_grid
 from ..trcwa.load_material_data import _load_material_data
 from ..materials.material_collection import MaterialCollection
+from ..materials.find_closest_material import _find_closest_material
 
 
 def plot_eps_per_point(model, run_cfg, compare_to_material=None):
@@ -19,20 +21,16 @@ def plot_eps_per_point(model, run_cfg, compare_to_material=None):
     """
     # Create epsilon grid from the model
     eps, _ = model_to_eps_grid(model, run_cfg)
-    eps = eps.detach().cpu().numpy()
+    eps_np = eps.detach().cpu().numpy()
+
+    material_collection = MaterialCollection(run_cfg.target_frequencies)
 
     # Load material data for comparison
     if compare_to_material is not None:
-        material_data = _load_material_data(
-            os.path.dirname(inspect.getfile(MaterialCollection))
-            + "/data/"
-            + compare_to_material
-            + ".csv",
-            run_cfg.target_frequencies,
-        )
+        material_data = material_collection[compare_to_material]
 
     # Create figure
-    fig = plt.figure(figsize=(10, 5), dpi=150)
+    fig = plt.figure(figsize=(10, 6), dpi=150)
     fig.patch.set_facecolor("white")
 
     # Plot epsilon
@@ -41,8 +39,8 @@ def plot_eps_per_point(model, run_cfg, compare_to_material=None):
     wl = freq_to_wl(run_cfg.target_frequencies)
 
     # Add some horizontal space
-    ax.set_xlim(wl[0], 2.5 * wl[-1])
-    ax2.set_xlim(wl[0], 2.5 * wl[-1])
+    ax.set_xlim(wl.min(), 2.5 * wl.max())
+    ax2.set_xlim(wl.min(), 2.5 * wl.max())
 
     ax.set_xlabel("Wavelength [µm]")
     ax.set_ylabel("Epsilon real part")
@@ -59,15 +57,41 @@ def plot_eps_per_point(model, run_cfg, compare_to_material=None):
     for x in range(eps.shape[0]):
         for y in range(eps.shape[1]):
             for N_layer in range(eps.shape[2]):
-                eps_point_real = eps[x, y, N_layer].real
-                eps_point_imag = eps[x, y, N_layer].imag
+                eps_point_real = eps_np[x, y, N_layer].real
+                eps_point_imag = eps_np[x, y, N_layer].imag
                 ax.plot(wl, eps_point_real, linewidth=1)
                 ax2.plot(wl, eps_point_imag, linewidth=1)
 
-                ax.text(wl[-1], eps_point_real[-1], f" {N_layer},{x},{y}", va="center")
-                ax2.text(wl[-1], eps_point_imag[-1], f" {N_layer},{x},{y}", va="center")
+                ax.text(
+                    wl.max(),
+                    eps_point_real[0],
+                    f" {N_layer},{x},{y}",
+                    va="center",
+                    fontsize=7,
+                )
+                ax2.text(
+                    wl.max(),
+                    eps_point_imag[0],
+                    f" {N_layer},{x},{y}",
+                    va="center",
+                    fontsize=7,
+                )
 
     # Plot material data
     if compare_to_material is not None:
         ax.plot(wl, material_data.real, "--", color="black", linewidth=1.5)
         ax2.plot(wl, material_data.imag, "--", color="black", linewidth=1.5)
+    else:
+        _, indices = _find_closest_material(eps, run_cfg)
+        unique_indices = torch.unique(indices)
+        names = [material_collection.material_names[i] for i in unique_indices]
+        for name in names:
+            material_data = material_collection[name]
+            ax.plot(wl, material_data.real, "--", color="black", linewidth=1.5)
+            ax2.plot(wl, material_data.imag, "--", color="black", linewidth=1.5)
+            ax.text(
+                wl.max(), material_data.real[0], " " + name, va="center", fontsize=7
+            )
+            ax2.text(
+                wl.max(), material_data.imag[0], " " + name, va="center", fontsize=7
+            )
