ae071ea9e4f1f2342c7c9962890af43581bcff7d
Pablo Gómez <contact@pablo-gomez.net>
2021-08-24 15:40:18 +0200

    Apply suggestions from code review
    Co-authored-by: Håvard Hem Toftevaag <70584234+htoftevaag@users.noreply.github.com>

--- nidn/materials/layer_builder.py
+++ nidn/materials/layer_builder.py
@@ -6,16 +6,16 @@ from .material_collection import MaterialCollection
 
 class LayerBuilder:
     def __init__(self, run_cfg) -> None:
-        """Initializes the builder
+        """Initializes the builder.
 
         Args:
-            run_cfg (DotMap): Run configuration
+            run_cfg (DotMap): Run configuration.
         """
         self.run_cfg = run_cfg
         self.material_collection = MaterialCollection(self.run_cfg.target_frequencies)
 
     def _setup_grid_and_materials(self,):
-        """Utility function to setup essentials
+        """Utility function to setup essentials.
         """
         # Get the grid ticks
         x0 = torch.linspace(0, 1, self.run_cfg.Nx)
@@ -33,10 +33,10 @@ class LayerBuilder:
         """Generates a uniform layer.
 
         Args:
-            grid_material (str): name of the material
+            grid_material (str): The name of the material.
 
         Returns:
-            torch.tensor: Tensor with epsilon values for the layer.
+            torch.tensor: Tensor with epsilon values for the layer
         """
         eps, _, _ = self._setup_grid_and_materials()
 
@@ -48,9 +48,9 @@ class LayerBuilder:
         """Generates a square layer with a square hole in the middle.
 
         Args:
-            grid_material (str): name of first material
-            square_material (str): name of second material
-            a (float, optional): Size of the square (0 to 1). Defaults to 0.33.
+            grid_material (str): Name of the grid material.
+            square_material (str): Name of the hole material.
+            a (float, optional): Size of the square hole (0 to 0.5). Defaults to 0.33.
 
         Returns:
             torch.tensor: Tensor with epsilon values for the layer.
@@ -72,17 +72,19 @@ class LayerBuilder:
         return eps
 
     def build_circle_layer(
-        self, grid_material, circle_material, radius=0.33, center=0.5
+        self, grid_material, circle_material, radius=0.33, x_center=0.5, y_center=0.5
     ):
-        """Generates a square layer with a square hole in the middle.
+        """Generates a square layer with a circular hole in the middle.
 
         Args:
-            grid_material (str): name of first material
-            circle_material (str): name of second material
-            a (float, optional): Size of the square (0 to 1). Defaults to 0.33.
+            grid_material (str): Name of the grid material.
+            circle_material (str): Name of the hole material.
+            a (float, optional): Radius of the circle (0 to 0.5). Defaults to 0.33.
+            x_center (float, optional): Center of the circle in the x-direction. Defaults to 0.5.
+            y_center (float, optional): Center of the circle in the y-direction. Defaults to 0.5.
 
         Returns:
-            torch.tensor: Tensor with epsilon values for the layer.
+            torch.tensor: Tensor with epsilon values for the layer
         """
         eps, grid_x, grid_y = self._setup_grid_and_materials()
 
