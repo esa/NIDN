97bc2ec94d23404ef6b4588aa00289a58bc015d4
torbjornstoro <to-bo-sto@hotmail.no>
2022-02-14 16:45:22 +0100

    First draft of completed files with docstrings, no testing performed yet

--- nidn/fdtd/compute_spectrum.py
+++ nidn/fdtd/compute_spectrum.py
@@ -1,7 +1,54 @@
+from weakref import ref
 from dotmap import DotMap
+import sys
+
+import torch
+from nidn.fdtd.calculate_transmission_reflection_coefficients import (
+    calculate_transmission_reflection_coefficients,
+)
+
+from nidn.trcwa.constants import TRCWA_EPS_0
+
+sys.path.append("../")
+import nidn
+from init_fdtd import init_fdtd
 
 
-def compute_spectrum(grid, cfg: DotMap):
+def compute_spectrum(cfg: DotMap):
+    """Generates a spectrum of transmission and reflection coefficients for the specified wavelengths in the cfg, by using FDTD simulations.
+
+    Args:
+        cfg (DotMap): Configurations needed to perform the simulations
+
+    Returns:
+        tuple[array, array]: Transmission spectrum and reflection spectrum
+    """
+    t_spectrum, r_spectrum = []
+    physical_wavelengths, norm_freq = nidn.get_frequency_points(cfg)
+
+    for w in physical_wavelengths:
+        transmission, reflection = []
+        grid, t_detector, r_detector = init_fdtd(cfg, False, w, permittivity=5.1984)
+        grid.run(cfg.FDTD_niter, progress_bar=False)
+        transmission[0], reflection[0] = _get_detector_values(
+            t_detector, r_detector, cfg.FDTD_grid[2] / 2
+        )
+
+        grid, t_detector, r_detector = init_fdtd(cfg, True, w, permittivity=5.1984)
+        grid.run(cfg.FDTD_niter, progress_bar=True)
+        transmission[1], reflection[1] = _get_detector_values(
+            t_detector, r_detector, cfg.FDTD_grid[2] / 2
+        )
+
+        (
+            transmission_coefficient,
+            reflection_coefficient,
+        ) = calculate_transmission_reflection_coefficients(
+            transmission, reflection, "MEAN SQUARE"
+        )
+        t_spectrum.append(transmission_coefficient)
+        r_spectrum.append(reflection_coefficient)
+
     # For each wavelength:
     # Run simulation
     # Get detector values
@@ -12,32 +59,46 @@ def compute_spectrum(grid, cfg: DotMap):
 
 
 def _get_detector_values(
-    grid, transmission_detector, reflection_detector, detector_value_y
+    transmission_detector, reflection_detector, detector_position_y
 ):
-    # Get values from the transmission detector and the reflection detector.
-    # This will be six lists for each detector, each list having either magnetic or electric field in either x,y or z direction
-    ex_transmission = transmission_detector.detector_values["E"][int(detector_value_y)][
-        0
-    ]
-    ey_transmission = transmission_detector.detector_values["E"][int(detector_value_y)][
-        1
-    ]
-    ez_transmission = transmission_detector.detector_values["E"][int(detector_value_y)][
-        2
-    ]
-    hx_transmission = transmission_detector.detector_values["H"][int(detector_value_y)][
-        0
-    ]
-    hy_transmission = transmission_detector.detector_values["H"][int(detector_value_y)][
-        1
-    ]
-    hz_transmission = transmission_detector.detector_values["H"][int(detector_value_y)][
-        2
-    ]
+    """Extract the signals detected by the transmission detector and the reflection detector
 
-    ex_reflection = reflection_detector.detector_values["E"][int(detector_value_y)][0]
-    ey_reflection = reflection_detector.detector_values["E"][int(detector_value_y)][1]
-    ez_reflection = reflection_detector.detector_values["E"][int(detector_value_y)][2]
-    hx_reflection = reflection_detector.detector_values["H"][int(detector_value_y)][0]
-    hy_reflection = reflection_detector.detector_values["H"][int(detector_value_y)][1]
-    hz_reflection = reflection_detector.detector_values["H"][int(detector_value_y)][2]
+    Args:
+        transmission_detector (fdtd.LineDetector): The transmission detector attached to the grid
+        reflection_detector (fdtd.LineDetector): The reflection detector attached to the grid
+        detector_position_y (int): The position along the linedetector which should be used
+
+    Returns:
+        tuple[array, array]: The electric field detected by the transmission detector, and the electric field detected by the reflection detector
+    """
+    e_transmission = _get_abs_value_from_3D_signal(
+        transmission_detector.detector_values["E"][int(detector_position_y)]
+    )
+    e_reflection = _get_abs_value_from_3D_signal(
+        reflection_detector.detector_values["E"][int(detector_position_y)]
+    )
+    h_transmission = _get_abs_value_from_3D_signal(
+        transmission_detector.detector_values["H"][int(detector_position_y)]
+    )
+    h_reflection = _get_abs_value_from_3D_signal(
+        reflection_detector.detector_values["H"][int(detector_position_y)]
+    )
+
+    return e_transmission, e_reflection
+
+
+def _get_abs_value_from_3D_signal(signal):
+    """Get the absolute value of a three-dimentional signal. For each time step, a =sqrt(x**2 + y**2 + z**2)
+
+    Args:
+        signal (array): Three dimentional time signal
+
+    Returns:
+        Array: One dimentional time-signal
+    """
+    abs_value = []
+    for i in range(len(signal)):
+        abs_value.append(
+            torch.sqrt(signal[i][0] ** 2 + signal[i][1] ** 2 + signal[i][2] ** 2)
+        )
+    return abs_value
--- nidn/fdtd/init_fdtd.py
+++ nidn/fdtd/init_fdtd.py
@@ -1,29 +1,80 @@
 from dotmap import DotMap
 import fdtd
-from dotmap import DotMap
 
-def init_fdtd(cfg: DotMap, include_object, period):
+from nidn.fdtd.constants import FDTD_UNIT_MAGNITUDE
+from nidn.utils.global_constants import SPEED_OF_LIGHT
+
+
+def init_fdtd(cfg: DotMap, include_object, wavelength, permittivity):
+    """Initialize the FDTD grid, with detectors, source, boundaries and optional objects
+
+    Args:
+        cfg (DotMap): configuration
+        include_object (bool): Whether an object should be added to the grid or not
+        wavelength (float): Wavelength to be used by the source
+        permittivity (array): array of permittivities of the object for each layer
+
+    Returns:
+        fdtd:Grid: Grid with all the added object, ready to be run
+    """
+    scaling = FDTD_UNIT_MAGNITUDE / (cfg.physical_wavelength_range[0] * 0.1)
     grid = fdtd.Grid(
-        (cfg.grid_size_x, cfg.grid_size_y, 1),
-        grid_spacing=cfg.grid_spacing,
+        (
+            int(cfg.FDTD_grid[0] * scaling),
+            int(
+                cfg.FDTD_grid[1] * scaling
+                + cfg.N_layers * scaling * cfg.FDTD_per_layer_thickness
+            ),
+            int(cfg.FDTD_grid[2] * scaling),
+        ),
+        grid_spacing=cfg.physical_wavelength_range[0] * 0.1,
         permittivity=1.0,
         permeability=1.0,
     )
-    grid = _add_boundaries(grid, cfg.pml_thickness)
+    grid = _add_boundaries(grid, int(cfg.FDTD_pml_thickness * scaling))
     grid, t_detector, r_detector = _add_detectors(
-        grid, cfg.transmission_detector_x, cfg.reflection_detector_x
+        grid,
+        int(
+            cfg.FDTD_reflection_detector_x * scaling
+            + cfg.N_layers * scaling * cfg.FDTD_per_layer_thickness
+        ),
+        int(cfg.FDTD_reflection_detector_x * scaling),
     )
     grid = _add_source(
-        grid, cfg.source_x, cfg.source_y, period, cfg.use_pulse_source, cfg.use_point_source
+        grid,
+        int(cfg.FDTD_source[0] * scaling),
+        int(cfg.FDTD_source[1] * scaling),
+        wavelength / SPEED_OF_LIGHT,
+        cfg.FDTD_use_pulsesource,
+        cfg.FDTD_use_pointsource,
     )
-    # TODO: Support multiple layers
     if include_object:
-        grid = _add_object(grid, cfg.object_start_x, cfg.object_end_x, cfg.permittivity)
+        for i in range(cfg.N_layers):
+            grid = _add_object(
+                grid,
+                int(
+                    cfg.FDTD_free_space_distance * scaling
+                    + i * scaling * cfg.FDTD_per_layer_thickness
+                ),
+                int(
+                    cfg.FDTD_object[1] * scaling
+                    + (i + 1) * scaling * cfg.FDTD_per_layer_thickness
+                ),
+                permittivity[i],
+            )
     return grid, t_detector, r_detector
 
 
 def _add_boundaries(grid, pml_thickness):
-    # Add theboundaries to the FDTD grid
+    """Add the desired boundaries to teh grid
+
+    Args:
+        grid (fdtd.Grid): The grid object which to add the boundaries
+        pml_thickness (float): Thickness of the PML boundaries,
+
+    Returns:
+        fdtd.Grid: The grid with the added boundaries
+    """
 
     grid[0:pml_thickness, :, :] = fdtd.PML(name="pml_xlow")
     grid[-pml_thickness:, :, :] = fdtd.PML(name="pml_xhigh")
@@ -32,7 +83,19 @@ def _add_boundaries(grid, pml_thickness):
 
 
 def _add_source(grid, source_x, source_y, period, use_pulse_source, use_point_source):
-    # Add the source to the FDTD grid
+    """Add a specified source to the fdtd grid
+
+    Args:
+        grid (fdtd.Grid): The grid object which to add the source
+        source_x (float): The x coordinates of the placement of the source
+        source_y (float): The y coordinates of the placement of the source
+        period (float): The period which the source should use, given by wavelength / speed_of_light
+        use_pulse_source (bool): True if pulse source is desired, Fasle to use continuous wave
+        use_point_source (bool): True if a point source is desired, False if a LineSource is desired
+
+    Returns:
+        fdtd.Grid: The grid with the added source
+    """
 
     if use_point_source:
         grid[source_x, source_y, 0] = fdtd.PointSource(
@@ -49,7 +112,16 @@ def _add_source(grid, source_x, source_y, period, use_pulse_source, use_point_so
 
 
 def _add_detectors(grid, transmission_detector_x, reflection_detector_x):
-    # Add detectors to the FDTD grid
+    """Add a transmission detector and reflection detector to the fdtd grid
+
+    Args:
+        grid (fdtd.Grid): The grid object which to add the detectors
+        transmission_detector_x (float): x coordinates of the transmission detector
+        reflection_detector_x (float): x coordinates of teh reflection detector
+
+    Returns:
+        fdtd.Grid: The grid with the added detectors
+    """
 
     transmission_detector = fdtd.LineDetector(name="t_detector")
     reflection_detector = fdtd.LineDetector(name="r_detector")
@@ -59,7 +131,17 @@ def _add_detectors(grid, transmission_detector_x, reflection_detector_x):
 
 
 def _add_object(grid, object_start_x, object_end_x, permittivity):
-    # Add an object to the FDTD grid
+    """Add a object to the fdtd grid, with a specified permittivity. The object covers the entire grid in the y-direction.
+
+    Args:
+        grid (fdtd:Grid): The grid object which to add the object
+        object_start_x (float): Coordinate where the object should start in the x-direction
+        object_end_x (float): Coordinate where the object should end in the x-direction
+        permittivity (float): The dielectric permittivity of the object
+
+    Returns:
+        fdtd.Grid: The grid with the added object
+    """
 
     grid[object_start_x:object_end_x, :, :] = fdtd.AnisotropicObject(
         permittivity=permittivity, name="object"
