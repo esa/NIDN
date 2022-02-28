3b4cc39c225596b0794f429c4e38f5f886f061a1
torbjornstoro <to-bo-sto@hotmail.no>
2022-03-01 00:13:33 +0100

    Reduced number of frequencies in fdtd simulations used in the tests

--- nidn/tests/fdtd_test.py
+++ nidn/tests/fdtd_test.py
@@ -65,6 +65,7 @@ def test_fdtd_simulation_single_layer():
     """Test that checks that the calculate_spectrum function returns the correct spectrum for a single layer"""
     # Create grid with uniform layer
     cfg = load_default_cfg()
+    cfg.N_freq = 5
     cfg.N_layers = 1
     eps_grid = zeros(1, 1, cfg.N_layers, cfg.N_freq, dtype=cfloat)
     # Note: something went wrong when smalles wavelength was 1e-5, guess its the grid scaling
@@ -80,46 +81,16 @@ def test_fdtd_simulation_single_layer():
     transmission_spectrum, reflection_spectrum = compute_spectrum(eps_grid, cfg)
     validated_transmission_spectrum = [
         tensor(0.0),
-        tensor(0.0),
-        tensor(2.0448e-308),
-        tensor(2.5918e-277),
-        tensor(3.8102e-132),
-        tensor(0.6251),
-        tensor(0.5362),
-        tensor(0.6362),
-        tensor(0.7259),
-        tensor(0.6041),
-        tensor(0.5613),
-        tensor(0.6453),
-        tensor(0.6915),
-        tensor(0.5796),
-        tensor(0.4658),
-        tensor(0.5885),
-        tensor(0.5805),
-        tensor(0.4043),
-        tensor(0.4957),
+        tensor(0.5564),
+        tensor(0.5902),
+        tensor(0.4664),
         tensor(0.4211),
     ]
     validated_reflection_spectrum = [
         tensor(0.9515),
-        tensor(0.8697),
-        tensor(0.8118),
-        tensor(0.7570),
-        tensor(0.6983),
-        tensor(0.2333),
-        tensor(0.3495),
-        tensor(0.2044),
-        tensor(0.0673),
-        tensor(0.2649),
-        tensor(0.3565),
-        tensor(0.2003),
-        tensor(0.0904),
-        tensor(0.2549),
-        tensor(0.3180),
-        tensor(0.1525),
-        tensor(0.1539),
-        tensor(0.3921),
-        tensor(0.2173),
+        tensor(0.1605),
+        tensor(0.3508),
+        tensor(0.3171),
         tensor(0.3437),
     ]
     assert all(
@@ -134,6 +105,7 @@ def test_fdtd_simulation_four_layers():
     cfg = load_default_cfg()
     cfg.N_layers = 4
     cfg.FDTD_niter = 400
+    cfg.N_freq = 5
     eps_grid = zeros(1, 1, cfg.N_layers, cfg.N_freq, dtype=cfloat)
     cfg.target_frequencies = compute_target_frequencies(
         cfg.physical_wavelength_range[0],
@@ -154,42 +126,12 @@ def test_fdtd_simulation_four_layers():
         tensor(0.0),
         tensor(0.0),
         tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
-        tensor(0.0),
     ]
     validated_reflection_spectrum = [
         tensor(0.9515),
-        tensor(0.8781),
-        tensor(0.8077),
-        tensor(0.7559),
-        tensor(0.7042),
-        tensor(0.5871),
-        tensor(0.5489),
-        tensor(0.6336),
-        tensor(0.4941),
-        tensor(0.6071),
-        tensor(0.4161),
-        tensor(0.6353),
-        tensor(0.5313),
-        tensor(0.6544),
-        tensor(0.6774),
-        tensor(0.4997),
-        tensor(0.6510),
-        tensor(0.4196),
-        tensor(0.5047),
+        tensor(0.4005),
+        tensor(0.4919),
+        tensor(0.6812),
         tensor(0.2888),
     ]
     assert all(
