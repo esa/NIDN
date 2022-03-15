Fixed gradient flow for FDTD, and changed tests thereby

nidn/fdtd_integration/calculate_transmission_reflection_coefficients.py
@@ -76,7 +76,7 @@ def _mean_square(arr):
     Returns:
         float: The mean square value
     """
-    return torch.tensor(sum([e**2 for e in arr]) / len(arr))
+    return torch.tensor(sum([e**2 for e in arr]) / len(arr), requires_grad=True)
 
 
 def _fft(signal, cfg: DotMap):

nidn/tests/fdtd_test.py
@@ -89,13 +89,17 @@ def test_fdtd_simulation_single_layer():
         [0.95146948, 0.17358959, 0.35082144, 0.31708776, 0.34369091]
     )
     assert all(
-        torch.abs(transmission_spectrum - validated_transmission_spectrum) < 1e-8
+        torch.abs(torch.tensor(transmission_spectrum) - validated_transmission_spectrum)
+        < 1e-8
     )
-    assert all(torch.abs(reflection_spectrum - validated_reflection_spectrum) < 1e-8)
-    assert all(transmission_spectrum <= 1)
-    assert all(reflection_spectrum <= 1)
-    assert all(transmission_spectrum >= 0)
-    assert all(reflection_spectrum >= 0)
+    assert all(
+        torch.abs(torch.tensor(reflection_spectrum) - validated_reflection_spectrum)
+        < 1e-8
+    )
+    assert all(e <= 1 for e in transmission_spectrum)
+    assert all(e <= 1 for e in reflection_spectrum)
+    assert all(e >= 0 for e in transmission_spectrum)
+    assert all(e >= 0 for e in reflection_spectrum)
 
 
 def test_fdtd_simulation_four_layers():
@@ -132,13 +136,17 @@ def test_fdtd_simulation_four_layers():
         [0.95147296, 0.51595957, 0.49191941, 0.68122679, 0.28879608]
     )
     assert all(
-        torch.abs(transmission_spectrum - validated_transmission_spectrum) < 1e-8
+        torch.abs(torch.tensor(transmission_spectrum) - validated_transmission_spectrum)
+        < 1e-8
     )
-    assert all(torch.abs(reflection_spectrum - validated_reflection_spectrum) < 1e-8)
-    assert all(transmission_spectrum <= 1)
-    assert all(reflection_spectrum <= 1)
-    assert all(transmission_spectrum >= 0)
-    assert all(reflection_spectrum >= 0)
+    assert all(
+        torch.abs(torch.tensor(reflection_spectrum) - validated_reflection_spectrum)
+        < 1e-8
+    )
+    assert all(e <= 1 for e in transmission_spectrum)
+    assert all(e <= 1 for e in reflection_spectrum)
+    assert all(e >= 0 for e in transmission_spectrum)
+    assert all(e >= 0 for e in reflection_spectrum)
 
 
 def test_single_patterned_layer():