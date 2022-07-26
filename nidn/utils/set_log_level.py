bf988bd55571f2590f2868c5f7e70b8d4f1ffeaa
Pablo Gómez <contact@pablo-gomez.net>
2022-07-26 17:17:04 +0200

    - Fixed log level setting if loglevel is specified lowercase

--- nidn/utils/set_log_level.py
+++ nidn/utils/set_log_level.py
@@ -12,8 +12,8 @@ def set_log_level(log_level: str):
     logger.add(
         sys.stderr,
         colorize=True,
-        level=log_level,
+        level=log_level.upper(),
         format="<green>{time:HH:mm:ss}</green>|NIDN-<blue>{level}</blue>| <level>{message}</level>",
         filter="nidn",
     )
-    logger.debug(f"Setting LogLevel to {log_level}")
+    logger.debug(f"Setting LogLevel to {log_level.upper()}")
